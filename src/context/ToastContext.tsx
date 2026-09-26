b1ba0b0f1d14871278fffae94a60a1d0c71f5642
waqas0324-dev <wasoboy23@gmail.com>
2026-09-26 23:24:34 +0500

    Move toast notifications to top of screen

--- src/context/ToastContext.tsx
+++ src/context/ToastContext.tsx
@@ -35,7 +35,7 @@ export function ToastProvider({ children }: { children: React.ReactNode }) {
     <ToastContext.Provider value={{ showToast }}>
       {children}
       {/* Toast Container */}
-      <div className="fixed bottom-6 right-6 z-[9999] flex flex-col gap-2 pointer-events-none">
+      <div className="fixed top-4 left-4 right-4 sm:left-auto sm:right-6 z-[9999] flex flex-col items-end gap-2 pointer-events-none">
         {toasts.map(toast => (
           <div
             key={toast.id}
